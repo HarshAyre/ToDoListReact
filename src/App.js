d8a8a934f406fd630825011cea1143aa523b80cc
HarshAyre <harshayre3@gmail.com>
2024-06-11 02:43:17 +0530

    my first todo list project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
-
+import { AiOutlineDelete } from "react-icons/ai";
+import { BsCheckLg } from "react-icons/bs";
 function App() {
+  const [isCompleteScreen, setIsCompleteScreen]=useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Todos</h1>
+      <div className='todo-wrapper'>
+        <div className='todo-input'>
+    
+        <div className='todo-input-item'>
+          <lable>Title:</lable> 
+          <input placeholder='what is the task' type='text' ></input>
+        </div>
+        
+        <div className='todo-input-item'>
+          <lable>Discription:</lable>
+          <input placeholder='specify discription of task' type='text' className=''></input>
+         </div>
+        <div className='todo-input-item'>
+        <button type='button' className='primaryBtn'> Add</button>
+        </div>
+       </div>
+
+        <div className='btn-area'>
+          <button className={`secondaryBtn ${isCompleteScreen===false && 'active'}`} onClick={()=>setIsCompleteScreen(false)}>Todo</button> 
+          <button className={`secondaryBtn ${isCompleteScreen===true && 'active'}`} onClick={()=>setIsCompleteScreen(true)}> Completed</button>
+        </div>
+       
+        <div className='todo-list'>
+          <div className='todo-list-item'>
+            <div><h3>Task 1</h3>
+            <p>Description</p></div>
+            
+            <div>
+            <AiOutlineDelete className='icon'/>
+            <BsCheckLg className='check-icon'/>
+            
+
+          </div>
+          </div>
+          
+        </div>
+      </div>
     </div>
   );
 }
